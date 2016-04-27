3b05fc89ae40d5cc0382b3d8775e03d9d75de050
Luis Baroni <lcsbaroni@yahoo.com.br>
2016-04-27 19:31:42 -0300

    Setup environment

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   resources :users
+
+  namespace :admin do
+    root 'users#index'
+    # resources :articles, :comments
+    resources :users
+  end
 end
